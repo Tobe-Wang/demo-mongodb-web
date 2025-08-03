c6b3886fe24b3ebb1acf55aa011108016f3007fb
Tobe-Wang <hzbeq@qq.com>
2025-08-03 23:10:30 +0800

    feat: 初始化 demo-mongodb-web项目
    - 创建项目结构和基本配置
    - 添加 Spring Boot 和 MongoDB 相关依赖
    - 设置 Maven 构建配置
    - 添加项目启动类和应用属性文件
    - 设置 .gitattributes 和 .gitignore 文件
    - 添加 README.md 项目说明文件

---
+++ README.md
@@ -0,0 +1,5 @@
+# demo-mongodb-web
+使用示例-Spring MongoDB
+> OpenJDK 21
+> 
+> Spring Boot 3
---
+++ src/main/java/cn/zhaofd/demomongodbweb/DemoMongodbWebApplication.java
@@ -0,0 +1,11 @@
+package cn.zhaofd.demomongodbweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMongodbWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMongodbWebApplication.class, args);
+    }
+}
